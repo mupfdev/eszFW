e9e22e66f8efbfcf50c292d781c0202c5381df61
Michael Fitzmayer <mail@michael-fitzmayer.de>
2020-08-13 15:41:11 +0200

    Move hash table to separate header file

--- src/esz.c
+++ src/esz.c
@@ -41,48 +41,11 @@ DISABLE_WARNING_SPECTRE_MITIGATION
 DISABLE_WARNING_POP
 
 #include "esz.h"
+#include "esz_hashes.h"
 #include "esz_types.h"
 
 DISABLE_WARNING_PUSH
 DISABLE_WARNING_SPECTRE_MITIGATION
-
-// Hash table
-// ----------------------------------------------------------------------------
-
-#define H_acceleration                 0xce26e518186a848f
-#define H_animated_tile_fps            0xf16ba347de2debdd
-#define H_background_constant_velocity 0x1cb19bb15ad8b7fc
-#define H_background_is_top_aligned    0xe10d87b900773f85
-#define H_background_layer_shift       0xf42f15f4c255007e
-#define H_climbable                    0x0377c455420b8600
-#define H_connect_horizontal_map_ends  0xb2d77d5c88cb679e
-#define H_connect_vertical_map_ends    0x8fd9bf6992bca50e
-#define H_actor                        0x000000310f128ebe
-#define H_gravitation                  0xc090e5ec12404d2d
-#define H_height                       0x0000065301d688de
-#define H_is_affected_by_gravity       0xd7df2608f228f6d1
-#define H_is_animated                  0xc09beeb13eae4983
-#define H_is_in_background             0xdba806855b4839b6
-#define H_is_in_foreground             0xdba8c1d8d43eb8f1
-#define H_is_in_midground              0x7b0525da0f6ff45f
-#define H_is_left_oriented             0x1af19f00c7d179a4
-#define H_is_moving                    0x0377cc4471f37f30
-#define H_is_player                    0x0377cc4478b16e8d
-#define H_jumping_power                0x702da8a7606d92ab
-#define H_max_velocity_x               0xa1d4b1b096163590
-#define H_meter_in_pixel               0xfbbc8a6d4a407cf9
-#define H_opengl                       0x0000065312ef9eea
-#define H_solid_above                  0xc0d1c0679690db0c
-#define H_solid_below                  0xc0d1c06796a48b18
-#define H_solid_left                   0x7272f650b702b58a
-#define H_solid_right                  0xc0d1c06797c82e7d
-#define H_sprite_sheet_id              0xe2141daae50cd180
-#define H_width                        0x0000003110a3b0a5
-
-#ifndef USE_LIBTMX // (cute_tiled.h)
-#define H_objectgroup                  0xc0b9d518970be349
-#define H_tilelayer                    0x0377d9f70e844fb0
-#endif
 
 // Private function prototypes
 // ----------------------------------------------------------------------------
